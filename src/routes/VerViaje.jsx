58ad7a1a37e2830fdcdaf05aa56aeb68094650ff
2000-jessi <jessica2_bece@yahoo.com>
2022-05-30 17:37:59 -0500

    Correccion clase

--- src/routes/VerViaje.jsx
+++ src/routes/VerViaje.jsx
@@ -3,20 +3,20 @@ import React from 'react'
 const VerViaje = () => {
   return (
     <div className="contenedor-verviaje">
-      <h1><i class="fas fa-user-check"></i> USUARIOS</h1>
+      <h1><i className="fas fa-user-check"></i> USUARIOS</h1>
       <div className="contenedor-tabla">
-        <table class="table">
-          <thead class="thead-dark">
+        <table className="table">
+          <thead className="thead-dark">
             <tr>
-              <th scope="col"><i class="fas fa-user"></i> Nombre</th>
-              <th scope="col"><i class="fas fa-users"></i> Cantidad Personas</th>
-              <th scope="col"><i class="fas fa-plane-departure"></i> Origen</th>
-              <th scope="col"><i class="fas fa-plane-arrival"></i> Destino</th>
-              <th scope="col"><i class="fas fa-id-card"></i> Clase</th>
-              <th scope="col"><i class="fas fa-calendar-alt"></i> Fecha Ida</th>
-              <th scope="col"><i class="fas fa-calendar-alt"></i> Fecha Vuelta</th>
-              <th scope="col"><i class="fas fa-edit"></i> Editar</th>
-              <th scope="col"><i class="fas fa-trash-alt"></i> Eliminar</th>
+              <th scope="col"><i className="fas fa-user"></i> Nombre</th>
+              <th scope="col"><i className="fas fa-users"></i> Cantidad Personas</th>
+              <th scope="col"><i className="fas fa-plane-departure"></i> Origen</th>
+              <th scope="col"><i className="fas fa-plane-arrival"></i> Destino</th>
+              <th scope="col"><i className="fas fa-id-card"></i> Clase</th>
+              <th scope="col"><i className="fas fa-calendar-alt"></i> Fecha Ida</th>
+              <th scope="col"><i className="fas fa-calendar-alt"></i> Fecha Vuelta</th>
+              <th scope="col"><i className="fas fa-edit"></i> Editar</th>
+              <th scope="col"><i className="fas fa-trash-alt"></i> Eliminar</th>
             </tr>
           </thead>
           <tbody>
@@ -29,10 +29,10 @@ const VerViaje = () => {
               <td>2021</td>
               <td>2024</td>
               <td>
-                <button type='button' className="boton-editar"><i class="fas fa-edit"></i></button>
+                <button type='button' className="boton-editar"><i className="fas fa-edit"></i></button>
               </td>                                    
               <td>
-                <button type='button' className="boton-eliminar"><i class="fas fa-trash-alt"></i></button>
+                <button type='button' className="boton-eliminar"><i className="fas fa-trash-alt"></i></button>
               </td>
             </tr>
             <tr>
@@ -44,10 +44,10 @@ const VerViaje = () => {
               <td>2021</td>
               <td>2024</td>
               <td>
-                <button type='button' className="boton-editar"><i class="fas fa-edit"></i></button>
+                <button type='button' className="boton-editar"><i className="fas fa-edit"></i></button>
               </td>                                    
               <td>
-                <button type='button' className="boton-eliminar"><i class="fas fa-trash-alt"></i></button>
+                <button type='button' className="boton-eliminar"><i className="fas fa-trash-alt"></i></button>
               </td>
             </tr>
           </tbody>    
