086ec938fe06536e6c276eaa0477aa705ef7f854
Alejandro Trejo Godinez <alejandro.trejog.15h@gmail.com>
2022-05-30 17:10:57 -0500

    Integrando API - POST

--- src/routes/Registrar.jsx
+++ src/routes/Registrar.jsx
@@ -1,53 +1,128 @@
 import React from 'react'
 
 const Registrar = () => {
-  return (
-    <div className="banner">
-        <div className="formularios">
-            <div className="contenedor-formulario">
-                <form action="">
-                    <div className="form">
-                        <p className="titulo">Reserva tu viaje</p>
-                        <div className="etiquetas">
-                            <p>DATOS</p>
+
+    const [nombreCompleto, setNombreCompleto] = React.useState("")
+    const [cantidad, setCantidad] = React.useState(null)
+    const [origen, setOrigen] = React.useState("")
+    const [destino, setDestino] = React.useState("")
+    const [clase, setClase] = React.useState("")
+    const [fechaIda, setFechaIda] = React.useState("")
+    const [fechaVuelta, setFechaVuelta] = React.useState("")
+
+    const Api = "https://api-react-firebase.herokuapp.com/api/reservations"
+
+    const registrarViaje = async () => {
+        if (nombreCompleto.trim() !== "" && cantidad.trim() !== null && origen !== "" && destino !== "" && clase !== "") {
+            const obj = {
+                nombre: nombreCompleto,
+                cantidadPersonas: cantidad,
+                origen: origen,
+                destino: destino,
+                clase: clase,
+                fechaIda: new Date(fechaIda),
+                fechaVuelta: new Date(fechaVuelta)
+            }
+            await fetch(Api, {
+                method: "POST",
+                headers: {
+                    'Content-Type': 'application/json;charset=utf-8'
+                },
+                body: JSON.stringify(obj)
+            })
+
+        } else {
+            alert("Falta llenar campos")
+        }
+    }
+
+    return (
+        <div className="banner">
+            <div className="formularios">
+                <div className="contenedor-formulario">
+                    <form action="">
+                        <div className="form">
+                            <p className="titulo">Reserva tu viaje</p>
+                            <div className="etiquetas">
+                                <p>DATOS</p>
+                            </div>
+                            <div className="grupo">
+                                <input
+                                    type="text"
+                                    name="txtNombre"
+                                    value={nombreCompleto}
+                                    onChange={event => { setNombreCompleto(event.target.value) }}
+                                />
+                                <span className="barra"></span>
+                                <label>Nombre Completo</label>
+                            </div>
+                            <div className="grupo">
+                                <input
+                                    type="number"
+                                    name="txtCantidad"
+                                    value={cantidad}
+                                    onChange={event => { setCantidad(event.target.value) }}
+                                /><span className="barra"></span>
+                                <label>Cantidad de personas</label>
+                            </div>
+                            <div className="grupo">
+                                <input
+                                    type="text"
+                                    name="txtOrigen"
+                                    value={origen}
+                                    onChange={event => { setOrigen(event.target.value) }}
+                                />
+                                <span className="barra"></span>
+                                <label>Origen</label>
+                            </div>
+                            <div className="grupo">
+                                <input
+                                    typeName="text"
+                                    name="txtDestino"
+                                    value={destino}
+                                    onChange={event => { setDestino(event.target.value) }}
+                                /><span className="barra"></span>
+                                <label>Destino</label>
+                            </div>
+                            <div className="grupo">
+                                <input
+                                    type="text"
+                                    name="txtClase"
+                                    value={clase}
+                                    onChange={event => { setClase(event.target.value) }}
+                                /><span className="barra"></span>
+                                <label>Clase</label>
+                            </div>
+                            <div className="grupo">
+                                <input
+                                    type="date"
+                                    name="txtDateIda"
+                                    value={fechaIda}
+                                    onChange={event => { setFechaIda(event.target.value) }}
+                                /><span className="barra"></span>
+                                <label>Fecha de Ida</label>
+                            </div>
+                            <div className="grupo">
+                                <input
+                                    type="date"
+                                    name="txtDateVuelta"
+                                    value={fechaVuelta}
+                                    onChange={event => { setFechaVuelta(event.target.value) }}
+                                /><span className="barra"></span>
+                                <label>Fecha de Vuelta</label>
+                            </div>
+                            <div className="boton-guardar">
+                                <button
+                                    type='button'
+                                    onClick={registrarViaje}
+                                >Guardar</button>
+                            </div>
                         </div>
-                        <div className="grupo">
-                            <input type="text" name="txtNombre"/><span className="barra"></span>
-                            <label>Nombre Completo</label>
-                        </div>
-                        <div className="grupo">
-                            <input type="number" name="txtCantidad"/><span className="barra"></span>
-                            <label>Cantidad de personas</label>
-                        </div>
-                        <div className="grupo">
-                            <input type="text" name="txtOrigen"/><span className="barra"></span>
-                            <label>Origen</label>
-                        </div>
-                        <div className="grupo">
-                            <input typeName="text" name="txtDestino"/><span className="barra"></span>
-                            <label>Destino</label>
-                        </div>
-                        <div className="grupo">
-                            <input type="text" name="txtClase"/><span className="barra"></span>
-                            <label>Clase</label>
-                        </div>
-                        <div className="grupo">
-                            <input type="date" name="txtDateIda"/><span className="barra"></span>
-                            <label>Fecha de Ida</label>
-                        </div>
-                        <div className="grupo">
-                            <input type="date" name="txtDateVuelta"/><span className="barra"></span>
-                            <label>Fecha de Vuelta</label>
-                        </div>
-                        <div className="boton-guardar">
-                            <button type='button'>Guardar</button>
-                        </div>
-                    </div>
-                </form>
+                    </form>
+                </div>
             </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default Registrar
