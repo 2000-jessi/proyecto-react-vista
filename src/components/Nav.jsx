66c2512abdbca50faaeb67980a47c0965af8da0c
Alejandro Trejo Godinez <alejandro.trejog.15h@gmail.com>
2022-05-30 20:24:34 -0500

    Mostrar datos en tabla funcionando

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -5,12 +5,12 @@ import { Link, NavLink } from 'react-router-dom'
 const Nav = () => {
   return (
     <nav>
-        <section className="contenedor-nav">
-            <div className="enlaces-header">
-                <Link to="/"><i class="fas fa-globe-americas"></i> Registrar viaje </Link>
-                <Link to="/verviaje"><i class="fas fa-plane-departure"></i> Ver mi viaje </Link>
-            </div>
-        </section>
+      <section className="contenedor-nav">
+        <div className="enlaces-header">
+          <Link to="/"><i className="fas fa-globe-americas"></i> Registrar viaje </Link>
+          <Link to="/verviaje"><i className="fas fa-plane-departure"></i> Ver mi viaje </Link>
+        </div>
+      </section>
     </nav>
   )
 }
